54998f7a67e02365f680ce5ec88fa76358b7e291
aaronp <aaronp@f1cf478b-080f-0410-abad-959bfeec9ea8>
2008-05-12 02:16:44 +0000

    using option inner text if there is no value
    git-svn-id: svn+ssh://rubyforge.org/var/svn/mechanize/trunk@486 f1cf478b-080f-0410-abad-959bfeec9ea8

---
+++ test/tc_option.rb
@@ -0,0 +1,17 @@
+require File.dirname(__FILE__) + "/helper"
+
+class OptionTest < Test::Unit::TestCase
+  class FakeAttribute < Hash
+    attr_reader :inner_text
+    def initialize(inner_text)
+      @inner_text = inner_text
+    end
+    alias :has_attribute? :has_key?
+  end
+
+  def test_option_missing_value
+    attribute = FakeAttribute.new('blah')
+    option = WWW::Mechanize::Form::Option.new(attribute, nil)
+    assert_equal('blah', option.value)
+  end
+end

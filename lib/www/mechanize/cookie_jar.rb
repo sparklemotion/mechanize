3b760bbf55ebbf13a1bce81ffa2084c0bc3ed886
aaronp <aaronp@f1cf478b-080f-0410-abad-959bfeec9ea8>
2008-05-12 02:16:36 +0000

    added mozilla cookie jar support thanks to Chris Riddoch
    git-svn-id: svn+ssh://rubyforge.org/var/svn/mechanize/trunk@485 f1cf478b-080f-0410-abad-959bfeec9ea8

--- lib/www/mechanize/cookie_jar.rb
+++ lib/www/mechanize/cookie_jar.rb
@@ -60,21 +60,112 @@ module WWW
         cookies
       end
 
-      # Save the cookie jar to a file as YAML
-      def save_as(file)
+      # Save the cookie jar to a file in the format specified.
+      #
+      # Available formats:
+      # :yaml  <- YAML structure
+      # :cookiestxt  <- Mozilla's cookies.txt format
+      def save_as(file, format = :yaml)
         ::File.open(file, "w") { |f|
-          YAML::dump(@jar, f)
+          case format
+          when :yaml:
+            YAML::dump(@jar, f)
+          when :cookiestxt:
+            dump_cookiestxt(f)
+          else
+            raise "Unknown cookie jar file format"
+          end
         }
       end
 
-      # Load cookie jar from a file stored as YAML
-      def load(file)
-        @jar = ::File.open(file) { |yf| YAML::load( yf ) }
+      # Load cookie jar from a file in the format specified.
+      #
+      # Available formats:
+      # :yaml  <- YAML structure.
+      # :cookiestxt  <- Mozilla's cookies.txt format
+      def load(file, format = :yaml)
+        @jar = ::File.open(file) { |f|
+          case format
+          when :yaml:
+            YAML::load(f)
+          when :cookiestxt:
+            load_cookiestxt(f)
+          else
+            raise "Unknown cookie jar file format"
+          end
+        }
       end
 
       # Clear the cookie jar
       def clear!
         @jar = {}
+      end
+
+
+      # Read cookies from Mozilla cookies.txt-style IO stream
+      def load_cookiestxt(io)
+        now = Time.now
+        fakeuri = Struct.new(:host)    # add_cookie wants something resembling a URI.
+        
+        io.each_line do |line|
+          line.chomp!
+          line.gsub!(/#.+/, '')
+          fields = line.split("\t")
+          
+          next if fields.length != 7
+          
+          expires_seconds = fields[4].to_i
+          begin
+            expires = Time.at(expires_seconds)
+          rescue
+            next
+            # Just in case we ever decide to support DateTime...
+            # expires = DateTime.new(1970,1,1) + ((expires_seconds + 1) / (60*60*24.0))
+          end
+          next if expires < now
+          
+          c = WWW::Mechanize::Cookie.new(fields[5], fields[6])
+          c.domain = fields[0]
+          # Field 1 indicates whether the cookie can be read by other machines at the same domain.
+          # This is computed by the cookie implementation, based on the domain value.
+          c.path = fields[2]               # Path for which the cookie is relevant
+          c.secure = (fields[3] == "TRUE") # Requires a secure connection
+          c.expires = expires             # Time the cookie expires.
+          c.version = 0                   # Conforms to Netscape cookie spec.
+          
+          add(fakeuri.new(c.domain), c)
+        end
+        @jar
+      end
+      
+      # Write cookies to Mozilla cookies.txt-style IO stream
+      def dump_cookiestxt(io)
+        @jar.each_pair do |domain, cookies|
+          cookies.each_pair do |name, cookie|
+            fields = []
+            fields[0] = cookie.domain
+            
+            if cookie.domain =~ /^\./
+              fields[1] = "TRUE"
+            else
+              fields[1] = "FALSE"
+            end
+            
+            fields[2] = cookie.path
+            
+            if cookie.secure == true
+              fields[3] = "TRUE"
+            else
+              fields[3] = "FALSE"
+            end
+            
+            fields[4] = cookie.expires.to_i.to_s
+            
+            fields[5] = cookie.name
+            fields[6] = cookie.value
+            io.puts(fields.join("\t"))
+          end 
+        end
       end
 
       private

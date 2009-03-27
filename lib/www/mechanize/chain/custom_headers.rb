7b04becc21b91baef09453a27eacfdec1cb1203e
Aaron Patterson <aaron.patterson@gmail.com>
2009-03-27 11:36:05 -0700

    overriding headers rather than adding fields. RF #24839

--- lib/www/mechanize/chain/custom_headers.rb
+++ lib/www/mechanize/chain/custom_headers.rb
@@ -8,11 +8,11 @@ module WWW
           request = params[:request]
           params[:headers].each do |k,v|
             case k
-            when :etag then request.add_field("ETag", v)
-            when :if_modified_since then request.add_field("If-Modified-Since", v)
+            when :etag then request["ETag"] = v
+            when :if_modified_since then request["If-Modified-Since"] = v
             else
               raise ArgumentError.new("unknown header symbol #{k}") if k.is_a? Symbol
-              request.add_field(k,v)
+              request[k] = v
             end
           end
           super

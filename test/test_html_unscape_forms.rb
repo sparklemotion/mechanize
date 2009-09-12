7a177bfe7b339e519db276ccc364ff0b2f39867b
Aaron Patterson <aaron.patterson@gmail.com>
2009-09-11 22:15:46 -0700

    fixing tests

--- test/test_html_unscape_forms.rb
+++ test/test_html_unscape_forms.rb
@@ -32,7 +32,7 @@ class TestCheckBoxes < Test::Unit::TestCase
   end
 
   def test_radio_button
-    f = Mechanize::Form::RadioButton.new('a&amp;b', 'a&amp;b', nil, nil)
+    f = Mechanize::Form::RadioButton.new('a&amp;b', 'a&amp;b', nil, nil, nil)
     assert_equal('a&b', f.name)
     assert_equal('a&b', f.value)
   end

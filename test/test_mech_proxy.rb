455b99c29ff6a36c2ad6a5e8bc80a9ed19203954
postmodern <postmodern.mod3@gmail.com>
2009-09-28 13:07:39 +0800

    Added a unit-test for WWW::Mechanize#set_proxy.

---
+++ test/test_mech_proxy.rb
@@ -0,0 +1,16 @@
+require File.expand_path(File.join(File.dirname(__FILE__), "helper"))
+
+class TestMechProxy< Test::Unit::TestCase
+  def setup
+    @agent = Mechanize.new
+  end
+
+  def test_set_proxy
+    @agent.set_proxy('www.example.com', 9001, 'joe', 'lol')
+
+    assert_equal(@agent.proxy_addr, 'www.example.com')
+    assert_equal(@agent.proxy_port, 9001)
+    assert_equal(@agent.proxy_user, 'joe')
+    assert_equal(@agent.proxy_pass, 'lol')
+  end
+end

b2e4d6b8bcbe2a7e3973b744ed4e0573443aabcd
Eric Hodel <drbrain@segment7.net>
2011-03-29 16:19:11 -0700

    Undent custom_headers

--- lib/mechanize/chain/custom_headers.rb
+++ lib/mechanize/chain/custom_headers.rb
@@ -1,21 +1,17 @@
-class Mechanize
-  class Chain
-    class CustomHeaders
-      include Mechanize::Handler
+class Mechanize::Chain::CustomHeaders
+  include Mechanize::Handler
 
-      def handle(ctx, params)
-        request = params[:request]
-        params[:headers].each do |k,v|
-          case k
-          when :etag then request["ETag"] = v
-          when :if_modified_since then request["If-Modified-Since"] = v
-          else
-            raise ArgumentError.new("unknown header symbol #{k}") if k.is_a? Symbol
-            request[k] = v
-          end
-        end
-        super
+  def handle(ctx, params)
+    request = params[:request]
+    params[:headers].each do |k,v|
+      case k
+      when :etag then request["ETag"] = v
+      when :if_modified_since then request["If-Modified-Since"] = v
+      else
+        raise ArgumentError.new("unknown header symbol #{k}") if k.is_a? Symbol
+        request[k] = v
       end
     end
+    super
   end
 end

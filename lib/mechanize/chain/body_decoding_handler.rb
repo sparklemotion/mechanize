73f3e630f7c80b2fbd92229e259d1d7e663e2022
Eric Hodel <drbrain@segment7.net>
2011-03-29 16:36:40 -0700

    Undent body_decoding_handler

--- lib/mechanize/chain/body_decoding_handler.rb
+++ lib/mechanize/chain/body_decoding_handler.rb
@@ -1,50 +1,46 @@
-class Mechanize
-  class Chain
-    class BodyDecodingHandler
-      include Mechanize::Handler
+class Mechanize::Chain::BodyDecodingHandler
+  include Mechanize::Handler
 
-      def handle(ctx, options)
-        body = options[:response_body]
-        response = options[:response]
+  def handle(ctx, options)
+    body = options[:response_body]
+    response = options[:response]
 
-        options[:response_body] =
-          if encoding = response['Content-Encoding']
-            case encoding.downcase
-            when 'gzip'
-              Mechanize.log.debug('gunzip body') if Mechanize.log
-              if response['Content-Length'].to_i > 0 || body.length > 0
-                begin
-                  Zlib::GzipReader.new(body).read
-                rescue Zlib::BufError, Zlib::GzipFile::Error
-                  if Mechanize.log
-                    Mechanize.log.error('Caught a Zlib::BufError')
-                  end
-                  body.rewind
-                  body.read(10)
-                  Zlib::Inflate.new(-Zlib::MAX_WBITS).inflate(body.read)
-                rescue Zlib::DataError
-                  if Mechanize.log
-                    Mechanize.log.error("Caught a Zlib::DataError, unable to decode page: #{$!.to_s}")
-                  end
-                  ''
-                end
-              else
-                ''
+    options[:response_body] =
+      if encoding = response['Content-Encoding']
+        case encoding.downcase
+        when 'gzip'
+          Mechanize.log.debug('gunzip body') if Mechanize.log
+          if response['Content-Length'].to_i > 0 || body.length > 0
+            begin
+              Zlib::GzipReader.new(body).read
+            rescue Zlib::BufError, Zlib::GzipFile::Error
+              if Mechanize.log
+                Mechanize.log.error('Caught a Zlib::BufError')
               end
-            when 'x-gzip'
-              body.read
-            when '7bit'
-              body.read
-            when 'none'
-              body.read
-            else
-              raise 'Unsupported content encoding'
+              body.rewind
+              body.read(10)
+              Zlib::Inflate.new(-Zlib::MAX_WBITS).inflate(body.read)
+            rescue Zlib::DataError
+              if Mechanize.log
+                Mechanize.log.error("Caught a Zlib::DataError, unable to decode page: #{$!.to_s}")
+              end
+              ''
             end
           else
-            body.read
+            ''
           end
-        super
+        when 'x-gzip'
+          body.read
+        when '7bit'
+          body.read
+        when 'none'
+          body.read
+        else
+          raise 'Unsupported content encoding'
+        end
+      else
+        body.read
       end
-    end
+    super
   end
 end

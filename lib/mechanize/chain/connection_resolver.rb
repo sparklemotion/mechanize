7e12a1a2a2c2e8012fc56541115d5e6a33b8b529
Eric Hodel <drbrain@segment7.net>
2011-03-29 16:21:03 -0700

    Undent connection_resolver

--- lib/mechanize/chain/connection_resolver.rb
+++ lib/mechanize/chain/connection_resolver.rb
@@ -1,28 +1,24 @@
-class Mechanize
-  class Chain
-    class ConnectionResolver
-      include Mechanize::Handler
+class Mechanize::Chain::ConnectionResolver
+  include Mechanize::Handler
 
-      def handle(ctx, params)
-        uri = params[:uri]
-        http_obj = nil
+  def handle(ctx, params)
+    uri = params[:uri]
+    http_obj = nil
 
-        case uri.scheme.downcase
-        when 'http', 'https' then
-          http_obj = ctx.http
-        when 'file' then
-          http_obj = Object.new
-          class << http_obj
-            def request(uri, request)
-              yield FileResponse.new(CGI.unescape(uri.path))
-            end
-          end
+    case uri.scheme.downcase
+    when 'http', 'https' then
+      http_obj = ctx.http
+    when 'file' then
+      http_obj = Object.new
+      class << http_obj
+        def request(uri, request)
+          yield Mechanize::FileResponse.new(CGI.unescape(uri.path))
         end
-
-        params[:connection] = http_obj
-
-        super
       end
     end
+
+    params[:connection] = http_obj
+
+    super
   end
 end

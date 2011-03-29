3d07f99d58e96fa44c4894ac3a60bbe5bab3e450
Eric Hodel <drbrain@segment7.net>
2011-03-29 16:33:21 -0700

    Undent response_body_parser

--- lib/mechanize/chain/response_body_parser.rb
+++ lib/mechanize/chain/response_body_parser.rb
@@ -1,36 +1,32 @@
-class Mechanize
-  class Chain
-    class ResponseBodyParser
-      include Mechanize::Handler
+class Mechanize::Chain::ResponseBodyParser
+  include Mechanize::Handler
 
-      def initialize(pluggable_parser, watch_for_set)
-        @pluggable_parser = pluggable_parser
-        @watch_for_set = watch_for_set
-      end
+  def initialize(pluggable_parser, watch_for_set)
+    @pluggable_parser = pluggable_parser
+    @watch_for_set = watch_for_set
+  end
 
-      def handle(ctx, params)
-        response = params[:response]
-        response_body = params[:response_body]
-        uri = params[:uri]
+  def handle(ctx, params)
+    response = params[:response]
+    response_body = params[:response_body]
+    uri = params[:uri]
 
-        content_type = nil
-        unless response['Content-Type'].nil?
-          data = response['Content-Type'].match(/^([^;]*)/)
-          content_type = data[1].downcase.split(',')[0] unless data.nil?
-        end
-
-        # Find our pluggable parser
-        parser_klass = @pluggable_parser.parser(content_type)
-        params[:page] = parser_klass.new(uri, response, response_body,
-                                   response.code) { |parser|
-          parser.mech = params[:agent] if parser.respond_to? :mech=
-          if parser.respond_to?(:watch_for_set=) && @watch_for_set
-            parser.watch_for_set = @watch_for_set
-          end
-        }
-
-        super
-      end
+    content_type = nil
+    unless response['Content-Type'].nil?
+      data = response['Content-Type'].match(/^([^;]*)/)
+      content_type = data[1].downcase.split(',')[0] unless data.nil?
     end
+
+    # Find our pluggable parser
+    parser_klass = @pluggable_parser.parser(content_type)
+    params[:page] = parser_klass.new(uri, response, response_body,
+                               response.code) { |parser|
+      parser.mech = params[:agent] if parser.respond_to? :mech=
+      if parser.respond_to?(:watch_for_set=) && @watch_for_set
+        parser.watch_for_set = @watch_for_set
+      end
+    }
+
+    super
   end
 end

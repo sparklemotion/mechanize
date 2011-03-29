5146519ef5b8a6b42e46a771cedc52b95ce122ee
Eric Hodel <drbrain@segment7.net>
2011-03-29 16:34:54 -0700

    Undent response_header_handler

--- lib/mechanize/chain/response_header_handler.rb
+++ lib/mechanize/chain/response_header_handler.rb
@@ -1,34 +1,30 @@
-class Mechanize
-  class Chain
-    class ResponseHeaderHandler
-      include Mechanize::Handler
+class Mechanize::Chain::ResponseHeaderHandler
+  include Mechanize::Handler
 
-      def initialize(cookie_jar)
-        @cookie_jar = cookie_jar
-      end
+  def initialize(cookie_jar)
+    @cookie_jar = cookie_jar
+  end
 
-      def handle(ctx, params)
-        response = params[:response]
-        uri = params[:uri]
-        page = params[:page]
+  def handle(ctx, params)
+    response = params[:response]
+    uri = params[:uri]
+    page = params[:page]
 
-        if page.is_a?(Page) && page.body =~ /Set-Cookie/n
-          page.search('//head/meta[@http-equiv="Set-Cookie"]').each do |meta|
-            Cookie::parse(uri, meta['content']) { |c|
-              Mechanize.log.debug("saved cookie: #{c}") if Mechanize.log
-              @cookie_jar.add(uri, c)
-            }
-          end
-        end
-
-        (response.get_fields('Set-Cookie')||[]).each do |cookie|
-          Cookie::parse(uri, cookie) { |c|
-            Mechanize.log.debug("saved cookie: #{c}") if Mechanize.log
-            @cookie_jar.add(uri, c)
-          }
-        end
-        super
+    if page.is_a?(Mechanize::Page) && page.body =~ /Set-Cookie/n
+      page.search('//head/meta[@http-equiv="Set-Cookie"]').each do |meta|
+        Mechanize::Cookie::parse(uri, meta['content']) { |c|
+          Mechanize.log.debug("saved cookie: #{c}") if Mechanize.log
+          @cookie_jar.add(uri, c)
+        }
       end
     end
+
+    (response.get_fields('Set-Cookie')||[]).each do |cookie|
+      Mechanize::Cookie::parse(uri, cookie) { |c|
+        Mechanize.log.debug("saved cookie: #{c}") if Mechanize.log
+        @cookie_jar.add(uri, c)
+      }
+    end
+    super
   end
 end

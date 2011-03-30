1b060c2228d91371639f5898180ea6450c0a9bdf
Eric Hodel <drbrain@segment7.net>
2011-03-29 20:14:57 -0700

    commit miss

--- lib/mechanize/chain.rb
+++ lib/mechanize/chain.rb
@@ -24,7 +24,6 @@ end
 require 'mechanize/chain/handler'
 require 'mechanize/chain/auth_headers'
 require 'mechanize/chain/body_decoding_handler'
-require 'mechanize/chain/connection_resolver'
 require 'mechanize/chain/custom_headers'
 require 'mechanize/chain/header_resolver'
 require 'mechanize/chain/pre_connect_hook'

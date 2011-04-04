cae018b8f08f08b2e9772e75dc03b384e45b96de
Eric Hodel <drbrain@segment7.net>
2011-04-03 17:11:28 -0700

    Undent

--- lib/mechanize/cookie.rb
+++ lib/mechanize/cookie.rb
@@ -1,84 +1,82 @@
 require 'time'
 require 'webrick/cookie'
 
-class Mechanize
-  # This class is used to represent an HTTP Cookie.
-  class Cookie < WEBrick::Cookie
+# This class is used to represent an HTTP Cookie.
+class Mechanize::Cookie < WEBrick::Cookie
 
-    attr_accessor :session
+  attr_accessor :session
 
-    def self.parse(uri, str, log = Mechanize.log)
-      return str.split(/,(?=[^;,]*=)|,$/).map { |c|
-        cookie_elem = c.split(/;+/)
-        first_elem = cookie_elem.shift
-        first_elem.strip!
-        key, value = first_elem.split(/\=/, 2)
+  def self.parse(uri, str, log = Mechanize.log)
+    return str.split(/,(?=[^;,]*=)|,$/).map { |c|
+      cookie_elem = c.split(/;+/)
+      first_elem = cookie_elem.shift
+      first_elem.strip!
+      key, value = first_elem.split(/\=/, 2)
 
-        cookie = nil
-        begin
-          cookie = new(key, value.dup)
-        rescue
-          log.warn("Couldn't parse key/value: #{first_elem}") if log
-        end
+      cookie = nil
+      begin
+        cookie = new(key, value.dup)
+      rescue
+        log.warn("Couldn't parse key/value: #{first_elem}") if log
+      end
 
-        next unless cookie
+      next unless cookie
 
-        cookie_elem.each do |pair|
-          pair.strip!
-          key, value = pair.split(/\=/, 2)
-          next unless key
-          value = WEBrick::HTTPUtils.dequote(value.strip) if value
+      cookie_elem.each do |pair|
+        pair.strip!
+        key, value = pair.split(/\=/, 2)
+        next unless key
+        value = WEBrick::HTTPUtils.dequote(value.strip) if value
 
-          case key.downcase
-          when "domain"  then cookie.domain  = value.sub(/^\./, '')
-          when "path"    then cookie.path    = value
-          when 'expires'
-            if value.empty? then
-              cookie.session = true
-              next
-            end
-
-            begin
-              cookie.expires = Time::parse(value)
-            rescue
-              log.warn("Couldn't parse expires: #{value}") if log
-            end
-          when "max-age" then
-            begin
-              cookie.max_age = Integer(value)
-            rescue
-              log.warn("Couldn't parse max age '#{value}'") if log
-              cookie.max_age = nil
-            end
-          when "comment" then cookie.comment = value
-          when "version" then
-            begin
-              cookie.version = Integer(value)
-            rescue
-              log.warn("Couldn't parse version '#{value}'") if log
-              cookie.version = nil
-            end
-          when "secure"  then cookie.secure = true
+        case key.downcase
+        when "domain"  then cookie.domain  = value.sub(/^\./, '')
+        when "path"    then cookie.path    = value
+        when 'expires'
+          if value.empty? then
+            cookie.session = true
+            next
           end
+
+          begin
+            cookie.expires = Time::parse(value)
+          rescue
+            log.warn("Couldn't parse expires: #{value}") if log
+          end
+        when "max-age" then
+          begin
+            cookie.max_age = Integer(value)
+          rescue
+            log.warn("Couldn't parse max age '#{value}'") if log
+            cookie.max_age = nil
+          end
+        when "comment" then cookie.comment = value
+        when "version" then
+          begin
+            cookie.version = Integer(value)
+          rescue
+            log.warn("Couldn't parse version '#{value}'") if log
+            cookie.version = nil
+          end
+        when "secure"  then cookie.secure = true
         end
+      end
 
-        cookie.path    ||= uri.path.to_s.sub(%r%[^/]*$%, '')
-        cookie.secure  ||= false
-        cookie.domain  ||= uri.host
-        # Move this in to the cookie jar
-        yield cookie if block_given?
+      cookie.path    ||= uri.path.to_s.sub(%r%[^/]*$%, '')
+      cookie.secure  ||= false
+      cookie.domain  ||= uri.host
+      # Move this in to the cookie jar
+      yield cookie if block_given?
 
-        cookie
-      }
-    end
+      cookie
+    }
+  end
 
-    def expired?
-      return false unless expires
-      Time.now > expires
-    end
+  def expired?
+    return false unless expires
+    Time.now > expires
+  end
 
-    def to_s
-      "#{@name}=#{@value}"
-    end
+  def to_s
+    "#{@name}=#{@value}"
   end
 end

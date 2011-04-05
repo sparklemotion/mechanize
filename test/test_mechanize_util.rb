b87c100c15a318508e15cc612fb6fcd418b8cd94
Eric Hodel <drbrain@segment7.net>
2011-04-05 16:58:19 -0700

    Fix test name for autotest

--- test/test_mechanize_util.rb
+++ test/test_mechanize_util.rb
@@ -1,6 +1,6 @@
 require "helper"
 
-class TestUtil < Test::Unit::TestCase
+class TestMechanizeUtil < Test::Unit::TestCase
   def test_from_native_charset
     assert_equal 'foo', Mechanize::Util.from_native_charset('foo', nil)
   end

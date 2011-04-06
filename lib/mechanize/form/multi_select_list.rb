cb5c32f4f8c358e95c51c84e2cc741830f77e2d0
Eric Hodel <drbrain@segment7.net>
2011-04-05 21:25:31 -0700

    Undent

--- lib/mechanize/form/multi_select_list.rb
+++ lib/mechanize/form/multi_select_list.rb
@@ -1,67 +1,67 @@
-class Mechanize
-  class Form
-    # This class represents a select list where multiple values can be selected.
-    # MultiSelectList#value= accepts an array, and those values are used as
-    # values for the select list.  For example, to select multiple values,
-    # simply do this:
-    #  list.value = ['one', 'two']
-    # Single values are still supported, so these two are the same:
-    #  list.value = ['one']
-    #  list.value = 'one'
-    class MultiSelectList < Field
-      attr_accessor :options
+# This class represents a select list where multiple values can be selected.
+# MultiSelectList#value= accepts an array, and those values are used as
+# values for the select list.  For example, to select multiple values,
+# simply do this:
+#
+#  list.value = ['one', 'two']
+#
+# Single values are still supported, so these two are the same:
+#
+#  list.value = ['one']
+#  list.value = 'one'
+class Mechanize::Form::MultiSelectList < Mechanize::Form::Field
+  attr_accessor :options
 
-      def initialize node
-        value = []
-        @options = []
+  def initialize node
+    value = []
+    @options = []
 
-        # parse
-        node.search('option').each do |n|
-          option = Option.new(n, self)
-          @options << option
-        end
-        super(node, value)
-      end
+    # parse
+    node.search('option').each do |n|
+      option = Option.new(n, self)
+      @options << option
+    end
+    super(node, value)
+  end
 
-      def query_value
-        value ? value.collect { |v| [name, v] } : ''
-      end
+  def query_value
+    value ? value.collect { |v| [name, v] } : ''
+  end
 
-      # Select no options
-      def select_none
-        @value = []
-        options.each { |o| o.untick }
-      end
+  # Select no options
+  def select_none
+    @value = []
+    options.each { |o| o.untick }
+  end
 
-      # Select all options
-      def select_all
-        @value = []
-        options.each { |o| o.tick }
-      end
+  # Select all options
+  def select_all
+    @value = []
+    options.each { |o| o.tick }
+  end
 
-      # Get a list of all selected options
-      def selected_options
-        @options.find_all { |o| o.selected? }
-      end
+  # Get a list of all selected options
+  def selected_options
+    @options.find_all { |o| o.selected? }
+  end
 
-      def value=(values)
-        select_none
-        [values].flatten.each do |value|
-          option = options.find { |o| o.value == value }
-          if option.nil?
-            @value.push(value)
-          else
-            option.select
-          end
-        end
-      end
-
-      def value
-        value = []
-        value.push(*@value)
-        value.push(*selected_options.collect { |o| o.value })
-        value
+  def value=(values)
+    select_none
+    [values].flatten.each do |value|
+      option = options.find { |o| o.value == value }
+      if option.nil?
+        @value.push(value)
+      else
+        option.select
       end
     end
   end
+
+  def value
+    value = []
+    value.push(*@value)
+    value.push(*selected_options.collect { |o| o.value })
+    value
+  end
+
 end
--- lib/mechanize/form/select_list.rb
+++ lib/mechanize/form/select_list.rb
@@ -1,68 +1,65 @@
-class Mechanize
-  class Form
-    # This class represents a select list or drop down box in a Form.  Set the
-    # value for the list by calling SelectList#value=.  SelectList contains a
-    # list of Option that were found.  After finding the correct option, set
-    # the select lists value to the option value:
-    #  selectlist.value = selectlist.options.first.value
-    # Options can also be selected by "clicking" or selecting them.  See Option
-    class SelectList < MultiSelectList
-      def initialize node
-        super
-        if selected_options.length > 1
-          selected_options.reverse[1..selected_options.length].each do |o|
-            o.unselect
-          end
-        end
-      end
-
-      ##
-      # Find all options on this select list with +criteria+
-      # Example:
-      #   select_list.options_with(:value => /1|2/).each do |field|
-      #     field.value = '20'
-      #   end
-      def options_with criteria
-        criteria = {:name => criteria} if String === criteria
-        f = @options.find_all do |thing|
-          criteria.all? { |k,v| v === thing.send(k) }
-        end
-        yield f if block_given?
-        f
-      end
-
-      ##
-      # Find one option on this select list with +criteria+
-      # Example:
-      #   select_list.option_with(:value => '1').value = 'foo'
-      def option_with criteria
-        f = options_with(criteria).first
-          yield f if block_given?
-        f
-      end
-
-      def value
-        value = super
-        if value.length > 0
-          value.last
-        elsif @options.length > 0
-          @options.first.value
-        else
-          nil
-        end
-      end
-
-      def value=(new)
-        if new != new.to_s and new.respond_to? :first
-          super([new.first])
-        else
-          super([new.to_s])
-        end
-      end
-
-      def query_value
-        value ? [[name, value]] : nil
+  # This class represents a select list or drop down box in a Form.  Set the
+  # value for the list by calling SelectList#value=.  SelectList contains a
+  # list of Option that were found.  After finding the correct option, set
+  # the select lists value to the option value:
+  #  selectlist.value = selectlist.options.first.value
+  # Options can also be selected by "clicking" or selecting them.  See Option
+class Mechanize::Form::SelectList < Mechanize::Form::MultiSelectList
+  def initialize node
+    super
+    if selected_options.length > 1
+      selected_options.reverse[1..selected_options.length].each do |o|
+        o.unselect
       end
     end
   end
+
+  ##
+  # Find all options on this select list with +criteria+
+  # Example:
+  #   select_list.options_with(:value => /1|2/).each do |field|
+  #     field.value = '20'
+  #   end
+  def options_with criteria
+    criteria = {:name => criteria} if String === criteria
+    f = @options.find_all do |thing|
+      criteria.all? { |k,v| v === thing.send(k) }
+    end
+    yield f if block_given?
+    f
+  end
+
+  ##
+  # Find one option on this select list with +criteria+
+  # Example:
+  #   select_list.option_with(:value => '1').value = 'foo'
+  def option_with criteria
+    f = options_with(criteria).first
+      yield f if block_given?
+    f
+  end
+
+  def value
+    value = super
+    if value.length > 0
+      value.last
+    elsif @options.length > 0
+      @options.first.value
+    else
+      nil
+    end
+  end
+
+  def value=(new)
+    if new != new.to_s and new.respond_to? :first
+      super([new.first])
+    else
+      super([new.to_s])
+    end
+  end
+
+  def query_value
+    value ? [[name, value]] : nil
+  end
 end
+

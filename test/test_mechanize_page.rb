b5bfedbc0f2241f20fbe5c15ea223be281498fd5
Eric Hodel <drbrain@segment7.net>
2011-04-08 19:28:26 -0700

    Decouple tests from Mechanize#get

--- test/test_mechanize_page.rb
+++ test/test_mechanize_page.rb
@@ -1,28 +1,47 @@
-require "helper"
-
+require 'helper'
 require 'cgi'
 
 class TestMechanizePage < Test::Unit::TestCase
+
+  WINDOWS_1255 = <<-HTML
+<meta http-equiv="content-type" content="text/html; charset=windows-1255">
+<title>hi</title>
+  HTML
+
+  BAD = <<-HTML
+<meta http-equiv="content-type" content="text/html; charset=windows-1255">
+<title>Bia\xB3ystok</title>
+  HTML
+
   def setup
     @agent = Mechanize.new
+    @uri = URI.parse 'http://example'
+    @res = { 'content-type' => 'text/html' }
+    @body = '<title>hi</title>'
+  end
+
+  def util_page body = @body, res = @res
+    Mechanize::Page.new @uri, res, body, 200, @agent
   end
 
   def test_encoding
-    page = @agent.get("http://localhost/tc_charset.html")
+    page = util_page WINDOWS_1255
+
     assert_equal 'windows-1255', page.encoding
   end
 
   def test_encoding_equals
-    page = @agent.get("http://localhost/file_upload.html")
+    page = util_page
 
     page.encoding = 'UTF-8'
 
+    assert_equal 'UTF-8', page.encoding
     assert_equal 'UTF-8', page.parser.encoding
   end
 
   def test_encoding_equals_before_parser
     # document has a bad encoding information - windows-1255
-    page = @agent.get("http://localhost/tc_bad_charset.html")
+    page = util_page BAD
 
     # encoding is wrong, so user wants to force ISO-8859-2
     page.encoding = 'ISO-8859-2'
@@ -33,7 +52,7 @@ class TestMechanizePage < Test::Unit::TestCase
 
   def test_encoding_equals_after_parser
     # document has a bad encoding information - windows-1255
-    page = @agent.get("http://localhost/tc_bad_charset.html")
+    page = util_page BAD
     page.parser
 
     # autodetection sets encoding to windows-1255
@@ -47,28 +66,27 @@ class TestMechanizePage < Test::Unit::TestCase
   end
 
   def test_title
-    page = @agent.get("http://localhost/file_upload.html")
-    assert_equal('File Upload Form', page.title)
+    page = util_page
+
+    assert_equal('hi', page.title)
   end
 
   def test_title_none
-    page = @agent.get("http://localhost/no_title_test.html")
+    page = util_page '' # invalid HTML
+
     assert_equal(nil, page.title)
   end
 
   def test_page_decoded_with_charset
-    page = Mechanize::Page.new(
-      URI.parse('http://tenderlovemaking.com/'),
-      { 'content-type' => 'text/html; charset=EUC-JP' },
-      '<html><body>hello</body></html>',
-      400,
-      @agent
-    )
+    page = util_page @body, 'content-type' => 'text/html; charset=EUC-JP'
+
+    assert_equal 'EUC-JP', page.encoding
     assert_equal 'EUC-JP', page.parser.encoding
   end
 
   def test_form
     page  = @agent.get("http://localhost/tc_form_action.html")
+
     form = page.form(:name => 'post_form1')
     assert form
     yielded = false
@@ -85,5 +103,6 @@ class TestMechanizePage < Test::Unit::TestCase
     assert form_by_action
     assert_equal(form, form_by_action)
   end
+
 end
 

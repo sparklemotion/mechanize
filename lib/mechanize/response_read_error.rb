359dab7e1c869b59a37ea892384e526fcfba1efa
Eric Hodel <drbrain@segment7.net>
2011-04-18 13:34:02 -0700

    Make it clear this is an IO, not a String

--- lib/mechanize/response_read_error.rb
+++ lib/mechanize/response_read_error.rb
@@ -5,18 +5,18 @@
 
 class Mechanize::ResponseReadError < Mechanize::Error
 
-  attr_reader :body
+  attr_reader :body_io
   attr_reader :error
   attr_reader :response
 
   ##
   # Creates a new ResponseReadError with the +error+ raised, the +response+
-  # and the +body+ read so far.
+  # and the +body_io+ for content read so far.
 
-  def initialize error, response, body
+  def initialize error, response, body_io
     @error = error
     @response = response
-    @body = body
+    @body_io = body_io
   end
 
   def message # :nodoc:

8ba92dfb7415b9bbe2141848340642cf940c26b1
Eric Hodel <drbrain@segment7.net>
2011-04-19 14:52:00 -0700

    Undent

--- lib/mechanize/page/meta.rb
+++ lib/mechanize/page/meta.rb
@@ -1,48 +1,44 @@
-class Mechanize
-  class Page < Mechanize::File
-    # This class encapsulates a Meta tag.  Mechanize treats meta tags just
-    # like 'a' tags.  Meta objects will contain links, but most likely will
-    # have no text.
-    class Meta < Link
+# This class encapsulates a Meta tag.  Mechanize treats meta tags just like
+# 'a' tags.  Meta objects will contain links, but most likely will have no
+# text.
 
-      # Matches the content attribute of a meta tag.  After the match:
-      #
-      #   $1:: delay
-      #   $3:: url
-      #
-      CONTENT_REGEXP = /^\s*(\d+\.?\d*)(;|;\s*url=\s*['"]?(\S*?)['"]?)?\s*$/i
+class Mechanize::Page::Meta < Mechanize::Page::Link
 
-      class << self
-        # Parses the delay and url from the content attribute of a meta tag.
-        # Parse requires the uri of the current page to infer a url when no
-        # url is specified.  If a block is given, the parsed delay and url
-        # will be passed to it for further processing.
-        #
-        # Returns nil if the delay and url cannot be parsed.
-        #
-        #   # <meta http-equiv="refresh" content="5;url=http://example.com/" />
-        #   uri = URI.parse('http://current.com/')
-        #
-        #   Meta.parse("5;url=http://example.com/", uri)  # => ['5', 'http://example.com/']
-        #   Meta.parse("5;url=", uri)                     # => ['5', 'http://current.com/']
-        #   Meta.parse("5", uri)                          # => ['5', 'http://current.com/']
-        #   Meta.parse("invalid content", uri)            # => nil
-        #
-        def parse(content, uri)
-          if content && content =~ CONTENT_REGEXP
-            delay, url = $1, $3
+  # Matches the content attribute of a meta tag.  After the match:
+  #
+  #   $1:: delay
+  #   $3:: url
+  #
+  CONTENT_REGEXP = /^\s*(\d+\.?\d*)(;|;\s*url=\s*['"]?(\S*?)['"]?)?\s*$/i
 
-            dest = uri
-            dest += url if url
-            url = dest.to_s
+  # Parses the delay and url from the content attribute of a meta tag.  Parse
+  # requires the uri of the current page to infer a url when no url is
+  # specified.  If a block is given, the parsed delay and url will be passed
+  # to it for further processing.
+  #
+  # Returns nil if the delay and url cannot be parsed.
+  #
+  #   # <meta http-equiv="refresh" content="5;url=http://example/" />
+  #   uri = URI.parse('http://example/')
+  #
+  #   Meta.parse("5;url=http://example/a", uri)  # => ['5', 'http://example/a']
+  #   Meta.parse("5;url=", uri)                  # => ['5', 'http://example/']
+  #   Meta.parse("5", uri)                       # => ['5', 'http://example/']
+  #   Meta.parse("invalid content", uri)         # => nil
+  #
+  def self.parse(content, uri)
+    if content && content =~ CONTENT_REGEXP
+      delay, url = $1, $3
 
-            block_given? ? yield(delay, url) : [delay, url]
-          else
-            nil
-          end
-        end
-      end
+      dest = uri
+      dest += url if url
+      url = dest.to_s
+
+      block_given? ? yield(delay, url) : [delay, url]
+    else
+      nil
     end
   end
+
 end
 

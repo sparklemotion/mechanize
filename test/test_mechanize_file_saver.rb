9975f00df6bd8882d63f06ee1146d6a01850b002
Eric Hodel <drbrain@segment7.net>
2011-10-20 13:43:25 -0700

    Name test after method

--- test/test_mechanize_file_saver.rb
+++ test/test_mechanize_file_saver.rb
@@ -2,31 +2,7 @@ require 'helper'
 
 class TestMechanizeFileSaver < MiniTest::Unit::TestCase
 
-  def test_file_saver_no_path
-    url = URI 'http://example.com'
-
-    fs = Mechanize::FileSaver.new(url, nil, 'hello world', 200)
-    assert_equal('example.com/index.html', fs.filename)
-    FileUtils.rm_rf('example.com')
-  end
-
-  def test_file_saver_slash
-    url = URI 'http://example.com/'
-
-    fs = Mechanize::FileSaver.new(url, nil, 'hello world', 200)
-    assert_equal('example.com/index.html', fs.filename)
-    FileUtils.rm_rf('example.com')
-  end
-
-  def test_file_saver_slash_file
-    url = URI 'http://example.com/foo.html'
-
-    fs = Mechanize::FileSaver.new(url, nil, 'hello world', 200)
-    assert_equal('example.com/foo.html', fs.filename)
-    FileUtils.rm_rf('example.com')
-  end
-
-  def test_file_saver_long_path_no_file
+  def test_initialize_long_path
     url = URI 'http://example.com/one/two/'
 
     fs = Mechanize::FileSaver.new(url, nil, 'hello world', 200)
@@ -34,11 +10,35 @@ class TestMechanizeFileSaver < MiniTest::Unit::TestCase
     FileUtils.rm_rf('example.com')
   end
 
-  def test_file_saver_long_path
+  def test_initialize_long_path_file
     url = URI 'http://example.com/one/two/foo.html'
 
     fs = Mechanize::FileSaver.new(url, nil, 'hello world', 200)
     assert_equal('example.com/one/two/foo.html', fs.filename)
+    FileUtils.rm_rf('example.com')
+  end
+
+  def test_initialize_no_path
+    url = URI 'http://example.com'
+
+    fs = Mechanize::FileSaver.new(url, nil, 'hello world', 200)
+    assert_equal('example.com/index.html', fs.filename)
+    FileUtils.rm_rf('example.com')
+  end
+
+  def test_initialize_slash
+    url = URI 'http://example.com/'
+
+    fs = Mechanize::FileSaver.new(url, nil, 'hello world', 200)
+    assert_equal('example.com/index.html', fs.filename)
+    FileUtils.rm_rf('example.com')
+  end
+
+  def test_initialize_slash_file
+    url = URI 'http://example.com/foo.html'
+
+    fs = Mechanize::FileSaver.new(url, nil, 'hello world', 200)
+    assert_equal('example.com/foo.html', fs.filename)
     FileUtils.rm_rf('example.com')
   end
 

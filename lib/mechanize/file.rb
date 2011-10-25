e6b043280f4a2f266b47877c961d0d1166e7aeb5
Eric Hodel <drbrain@segment7.net>
2011-10-24 19:48:24 -0700

    Document Mechanize::File.  Inspiration by Steven R Baker

--- lib/mechanize/file.rb
+++ lib/mechanize/file.rb
@@ -1,32 +1,94 @@
 ##
-# = Synopsis
-# This is the default (and base) class for the Pluggable Parsers.  If
-# Mechanize cannot find an appropriate class to use for the content type,
-# this class will be used.  For example, if you download a JPG, Mechanize
-# will not know how to parse it, so this class will be instantiated.
+# This is the base class for the Pluggable Parsers.  If Mechanize cannot find
+# an appropriate class to use for the content type, this class will be used.
+# For example, if you download an image/jpeg, Mechanize will not know how to
+# parse it, so this class will be instantiated.
 #
 # This is a good class to use as the base class for building your own
 # pluggable parsers.
 #
 # == Example
-#   require 'rubygems'
+#
 #   require 'mechanize'
 #
 #   agent = Mechanize.new
 #   agent.get('http://example.com/foo.jpg').class  #=> Mechanize::File
 
 class Mechanize::File
+
   extend Forwardable
 
-  attr_accessor :uri, :response, :body, :code, :filename
+  ##
+  # The URI this file was retrieved from
+
+  attr_accessor :uri
+
+  ##
+  # The Net::HTTPResponse for this file
+
+  attr_accessor :response
+
+  ##
+  # The HTTP response body, the raw file contents
+
+  attr_accessor :body
+
+  ##
+  # The HTTP response code
+
+  attr_accessor :code
+
+  ##
+  # The filename for this file based on the content-disposition of the
+  # response or the basename of the URL
+
+  attr_accessor :filename
+
+  ##
+  # Alias for the HTTP response object
+
   alias :header :response
+
+  ##
+  # :method: [](header)
+  #
+  # Access HTTP +header+ by name
+
   def_delegator :header, :[], :[]
+
+  ##
+  # :method: []=(header, value)
+  #
+  # Set HTTP +header+ to +value+
+
   def_delegator :header, :[]=, :[]=
+
+  ##
+  # :method: key?(header)
+  #
+  # Is the named +header+ present?
+
   def_delegator :header, :key?, :key?
+
+  ##
+  # :method: each
+  #
+  # Enumerate HTTP headers
+
   def_delegator :header, :each, :each
+
+  ##
+  # :method: each
+  #
+  # Enumerate HTTP headers in capitalized (canonical) form
+
   def_delegator :header, :canonical_each, :canonical_each
 
   alias :content :body
+
+  ##
+  # Creates a new file retrieved from the given +uri+ and +response+ object.
+  # The +body+ is the HTTP response body and +code+ is the HTTP status.
 
   def initialize(uri=nil, response=nil, body=nil, code=nil)
     @uri = uri
@@ -59,7 +121,9 @@ class Mechanize::File
     yield self if block_given?
   end
 
-  # Use this method to save the content of this object to filename
+  ##
+  # Use this method to save the content of this object to +filename+
+
   def save_as(filename = nil)
     if filename.nil?
       filename = @filename
@@ -76,5 +140,6 @@ class Mechanize::File
   end
 
   alias :save :save_as
+
 end
 

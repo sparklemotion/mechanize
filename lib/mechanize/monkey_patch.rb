c469c222bb67e94fc1a0f0e83795e79c8bd6c521
Eric Hodel <drbrain@segment7.net>
2011-11-01 17:54:04 -0700

    Undent net/http monkey patch for 1.8

--- lib/mechanize/monkey_patch.rb
+++ lib/mechanize/monkey_patch.rb
@@ -1,17 +1,16 @@
-if RUBY_VERSION < '1.9' then
-  module Net
-    class HTTP
-      alias :old_keep_alive? :keep_alive?
-      def keep_alive?(req, res)
-        return false if /close/i =~ req['connection'].to_s
-        return false if @seems_1_0_server
-        return false if /close/i      =~ res['connection'].to_s
-        return true  if /keep-alive/i =~ res['connection'].to_s
-        return false if /close/i      =~ res['proxy-connection'].to_s
-        return true  if /keep-alive/i =~ res['proxy-connection'].to_s
-        (@curr_http_version == '1.1')
-      end
-    end
+# :stopdoc:
+
+class Net::HTTP
+  alias old_keep_alive? keep_alive?
+
+  def keep_alive?(req, res)
+    return false if /close/i =~ req['connection'].to_s
+    return false if @seems_1_0_server
+    return false if /close/i      =~ res['connection'].to_s
+    return true  if /keep-alive/i =~ res['connection'].to_s
+    return false if /close/i      =~ res['proxy-connection'].to_s
+    return true  if /keep-alive/i =~ res['proxy-connection'].to_s
+    (@curr_http_version == '1.1')
   end
-end
+end if RUBY_VERSION < '1.9'
 

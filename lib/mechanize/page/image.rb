de0da2a2e8936c096d35453a654aa2b6c17f6f2c
kitamomonga <kitamomonga@gmail.com>
2011-11-29 08:42:06 +0900

    new methods: Page#image(s)_with, Image#alt(and major attrs), Image#caption, Image#pretty_print
      agent.page.image_with(:alt => 'alt-string')
      agent.page.image_with(:title => 'image title')
    
    Image#caption returns "ALT" when <img alt="ALT">
    Image#caption returns "TITLE" when <img title="TITLE">
    Image#caption returns "TITLE" when <img alt="ALT" title="TITLE">
    Image#caption returns "" when <img> (no alt nor title)
    
      p page.images[0] #=> #<Mechanize::Page::Image "src-url" "caption">

--- lib/mechanize/page/image.rb
+++ lib/mechanize/page/image.rb
@@ -4,15 +4,55 @@
 class Mechanize::Page::Image
   attr_reader :node
   attr_reader :page
+  attr_reader :mech
 
   def initialize(node, page)
     @node = node
     @page = page
+    @mech = page.mech
   end
 
   def src
-    @node['src']
+    node['src']
   end
+
+  def width
+    node['width']
+  end
+
+  def height
+    node['height']
+  end
+
+  def alt
+    node['alt']
+  end
+
+  def title
+    node['title']
+  end
+
+  # 'id' attribute of the image
+  def dom_id
+    node['id']
+  end
+
+  # 'class' attribute of the image
+  def dom_class
+    node['class']
+  end
+
+  # "caption" of the image. #title, #alt, or empty string "".
+  #   <img src="..." alt="ALT"> ==> "ALT"
+  #   <img src="..." title="TITLE"> ==> "TITLE"
+  #   <img src="..." alt="ALT" title="TITLE"> ==> "TITLE"
+  #   <img src="..." alt="no-popup-ALT" title=""> ==> ""
+  #   <img src="..."> ==> ""
+  def caption
+    title || alt || ''
+  end
+
+  alias :text :caption
 
   def url
     case src
@@ -26,5 +66,17 @@ class Mechanize::Page::Image
       end
     end
   end
+
+  alias :to_s :url
+
+  def pretty_print(q) # :nodoc:
+    q.object_group(self) {
+      q.breakable; q.pp url
+      q.breakable; q.pp caption
+    }
+  end
+
+  alias inspect pretty_inspect # :nodoc:
+
 end
 
--- test/test_mechanize_page_image.rb
+++ test/test_mechanize_page_image.rb
@@ -8,15 +8,63 @@ class TestMechanizePageImage < Mechanize::TestCase
     @uri = URI 'http://example/'
   end
 
-  def test_images
+  def test_image_attributes
+    page = html_page <<-BODY
+<img src="a.jpg" alt="alt" width="100" height="200" title="title" id="id" class="class">
+    BODY
+    assert_equal "a.jpg", page.images.first.src
+    assert_equal "alt", page.images.first.alt
+    assert_equal "100", page.images.first.width
+    assert_equal "200", page.images.first.height
+    assert_equal "title", page.images.first.title
+    assert_equal "id", page.images.first.dom_id
+    assert_equal "class", page.images.first.dom_class
+  end
+
+  def test_image_attributes_nil
+    page = html_page <<-BODY
+<img>
+    BODY
+    assert_nil page.images.first.src
+    assert_nil page.images.first.alt
+    assert_nil page.images.first.width
+    assert_nil page.images.first.height
+    assert_nil page.images.first.title
+    assert_nil page.images.first.dom_id
+    assert_nil page.images.first.dom_class
+  end
+
+  def test_image_caption
+    page = html_page <<-BODY
+<img src="a.jpg" alt="alt">
+    BODY
+    assert_equal "alt", page.images.first.caption
+
+    page = html_page <<-BODY
+<img src="a.jpg" title="title">
+    BODY
+    assert_equal "title", page.images.first.caption
+
+    page = html_page <<-BODY
+<img src="a.jpg" alt="alt" title="title">
+    BODY
+    assert_equal "title", page.images.first.caption
+
+    page = html_page <<-BODY
+<img src="a.jpg">
+    BODY
+    assert_equal "", page.images.first.caption
+  end
+
+  def test_image_url
     page = html_page <<-BODY
 <img src="a.jpg">
     BODY
 
-    assert_equal page.images.first.url, "http://example/a.jpg"
+    assert_equal "http://example/a.jpg", page.images.first.url
   end
 
-  def test_images_base
+  def test_image_url_base
     page = html_page <<-BODY
 <head>
   <base href="http://other.example/">
@@ -26,8 +74,28 @@ class TestMechanizePageImage < Mechanize::TestCase
 </body>
     BODY
 
-    assert_equal page.images.first.url, "http://other.example/a.jpg"
+    assert_equal "http://other.example/a.jpg", page.images.first.url
+  end
+
+  def test_image_with
+    page = html_page <<-BODY
+<img src="a.jpg">
+<img src="b.jpg">
+<img src="c.png">
+    BODY
+
+    assert_equal "http://example/b.jpg", page.image_with(:src => 'b.jpg').url
+  end
+
+  def test_images_with
+    page = html_page <<-BODY
+<img src="a.jpg">
+<img src="b.jpg">
+<img src="c.png">
+    BODY
+
+    images = page.images_with(:src => /jpg\Z/).map{|img| img.url}
+    assert_equal ["http://example/a.jpg", "http://example/b.jpg"], images
   end
 
 end
-

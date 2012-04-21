c39c1d2bb4ab5896e01ea0beb1214b88632004e8
Eric Hodel <drbrain@segment7.net>
2012-04-20 17:37:08 -0700

    Fixed Mechanize::ResponseCodeError#to_s when the page is actually an HTTPUnknownResponse and has no URI

--- lib/mechanize/response_code_error.rb
+++ lib/mechanize/response_code_error.rb
@@ -15,8 +15,10 @@ class Mechanize::ResponseCodeError < Mechanize::Error
   end
 
   def to_s
-    "#{@response_code} => #{Net::HTTPResponse::CODE_TO_OBJ[@response_code]} " \
-    "for #{@page.uri} -- #{super}"
+    response_class = Net::HTTPResponse::CODE_TO_OBJ[@response_code]
+    out = "#{@response_code} => #{response_class} "
+    out << "for #{@page.uri} " if @page.respond_to? :uri # may be HTTPResponse
+    out << "-- #{super}"
   end
 
   alias inspect to_s

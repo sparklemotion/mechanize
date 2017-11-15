a4fe523c6be2ea04c82686275845d590efd7f34e
weshatheleopard <github@wesha.name>
2017-11-15 21:58:36 +0000

    Move socksification out of the main body

--- lib/mechanize/http/agent.rb
+++ lib/mechanize/http/agent.rb
@@ -2,8 +2,6 @@ require 'tempfile'
 require 'net/ntlm'
 require 'kconv'
 require 'webrobots'
-require "socksify"
-require 'socksify/http'
 
 ##
 # An HTTP (and local disk access) user agent.  This class is an implementation
@@ -325,19 +323,6 @@ class Mechanize::HTTP::Agent
         raise Mechanize::ResponseCodeError.new(page, 'unhandled response')
       end
     end
-  end
-
-  def set_socks addr, port
-    set_http unless @http
-    class << @http
-      attr_accessor :socks_addr, :socks_port
- 
-      def http_class
-        Net::HTTP.SOCKSProxy(socks_addr, socks_port)
-      end
-    end
-    @http.socks_addr = addr
-    @http.socks_port = port
   end
 
   # URI for a proxy connection

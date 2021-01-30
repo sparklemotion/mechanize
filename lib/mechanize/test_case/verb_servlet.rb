5b30aed33cbac9825e8978f8e36dd221cbd4c093
Mike Dalessio <mike.dalessio@gmail.com>
2021-01-30 12:35:34 -0500

    test: remove rubocop security warnings from TestCase
    - change implicit Kernel.open to ::File.open
    - replace `eval` with `define_method`

--- lib/mechanize/test_case/verb_servlet.rb
+++ lib/mechanize/test_case/verb_servlet.rb
@@ -1,11 +1,9 @@
 class VerbServlet < WEBrick::HTTPServlet::AbstractServlet
   %w[HEAD GET POST PUT DELETE].each do |verb|
-    eval <<-METHOD
-      def do_#{verb}(req, res)
-        res.header['X-Request-Method'] = #{verb.dump}
-        res.body = #{verb.dump}
-      end
-    METHOD
+    define_method "do_#{verb}" do |req, res|
+      res.header['X-Request-Method'] = verb
+      res.body = verb
+    end
   end
 end
 

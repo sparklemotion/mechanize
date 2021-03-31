cb4b885f20703301980eb7b55bae917d7c15e5ef
Mike Dalessio <mike.dalessio@gmail.com>
2021-03-31 15:57:30 -0400

    feat: FileConnection supports Windows drive letters in the path
    Closes #483

--- test/test_mechanize_file_connection.rb
+++ test/test_mechanize_file_connection.rb
@@ -19,5 +19,22 @@ class TestMechanizeFileConnection < Mechanize::TestCase
     assert_equal File.read(__FILE__), body
   end
 
+  def test_request_on_uri_with_windows_drive
+    uri_string = "file://C:/path/to/file.html"
+    expected_file_path = "C:/path/to/file.html"
+
+    uri = URI.parse(uri_string)
+    conn = Mechanize::FileConnection.new
+
+    called = false
+    yielded_file_path = nil
+    conn.request(uri, nil) do |response|
+      called = true
+      yielded_file_path = response.file_path
+    end
+
+    assert(called)
+    assert_equal(expected_file_path, yielded_file_path)
+  end
 end
 
